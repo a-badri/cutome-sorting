8a7461ec20517f558f380721304935a46352b4ab
badri <ahmed.dope@gmail.com>
2022-01-25 13:07:07 -0600

    first-commit

---
+++ custom-sorts.js
@@ -0,0 +1,28 @@
+function ageSort(users) {
+  // Your code here
+  
+}
+
+function oddEvenSort(arr) {
+  // Your code here
+}
+
+function validAnagrams(s, t) {
+  // Your code here
+}
+
+function reverseBaseSort(arr) {
+  // Your code here
+}
+
+function frequencySort(arr) {
+  // Your code here
+}
+
+module.exports = [
+  oddEvenSort,
+  validAnagrams,
+  reverseBaseSort,
+  frequencySort,
+  ageSort,
+];
